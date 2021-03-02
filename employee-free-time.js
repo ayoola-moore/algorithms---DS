00381c63677d688c680e440de3974868f2908cb8
Ayoola <ayoola@transmissionmedia.ca>
2021-03-02 09:02:54 +0400

    problem resolved

--- employee-free-time.js
+++ employee-free-time.js
@@ -1,28 +1,41 @@
 function findEmployeeFreeTime(intervals) {
     const employeeFreeTime = []
-
-    let earliestTime= Infinity, latestTime = 0;
-
-    for (let i = 0; i < intervals.length; i++) { // [ [[]],[[]] ]
-       const currentEmployeeTimeIntervals = intervals[i] ;
-
-       
-       let startTimeA = currentEmployeeTimeIntervals[0][0] // eg 5
-       let endTimeA   = currentEmployeeTimeIntervals[0][1]
-       
-       if(currentEmployeeTimeIntervals.length > 1){
-
-           for (let j = 1; j < currentEmployeeTimeIntervals.length; j++) { // [[]]
-               const timeIntervals = currentEmployeeTimeIntervals[j]; //[]
-               const startTimeB = timeIntervals[0]
-               const endTimeB = timeIntervals[1]
-               //find time interval
-               const intervals = [].push([endTimeA, startTimeB])
-               startTimeA = startTimeB;
-               endTimeB = endTimeB
-            }
-        }
-        earliestTime = Math.min(earliestTime, startTime)
-        latestTime   = Math.min(latestTime, endTime)
+    // combine all intervals
+    let combinedIntervals = []
+    for (let i = 0; i < intervals.length; i++) {
+        combinedIntervals.push(...intervals[i])
     }
+    combinedIntervals = combinedIntervals.sort((a, b) => a[0] - b[0])
+
+    // merge all intervals
+    const mergedIntervals = [combinedIntervals[0]]
+
+    for (let j = 1; j < combinedIntervals.length; j++) {
+        
+        let a = mergedIntervals[mergedIntervals.length-1], aStart = a[0], aEnd = a[1];
+        
+        let b = combinedIntervals[j], bStart= b[0], bEnd = b[1];
+
+        if (aEnd >= bStart) {
+            a[1] = Math.max(aEnd, bEnd)
+        } else {
+            mergedIntervals.push(b)
+        }
+    }
+
+    // find the common interval gap(s) that will determine free time
+    for (let k = 1; k < mergedIntervals.length; k++) {
+        const time = mergedIntervals;
+        if (time[k - 1][1] <= time[k][0]) {
+            const freeInterval = [time[k - 1][1], time[k][0]]
+            employeeFreeTime.push(freeInterval)
+        }
+
+    }
+
+    return employeeFreeTime
 }
+
+console.log(findEmployeeFreeTime([[[1,3]],[[2,4]],[[3,5],[7,9]]]));
+console.log(findEmployeeFreeTime([[[1, 3], [5, 6]], [[2, 3], [6, 8]]]));
+console.log(findEmployeeFreeTime([[[1,3]],[[9,12]],[[2,4],[6,8]]]));
